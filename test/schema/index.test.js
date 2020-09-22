d64d960b6e4890bf22b10e5e09c84d1886a07ed6
Rich <rich.gwozdz@gmail.com>
2020-09-22 12:05:06 -0700

    Implement time coercion.

--- test/schema/index.test.js
+++ test/schema/index.test.js
@@ -1,38 +1,45 @@
 /* eslint-env mocha */
 const chai = require('chai')
 const expect = chai.expect
-const { routeParamsSchema } = require('../../lib/schema')
+const { paramsSchema } = require('../../lib/schema')
+const iso8601Regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/
 
 describe('schema', function () {
-  describe('routeParamsSchema', function () {
+  describe('paramsSchema', function () {
     it('should reject invalid metric param', () => {
-      const { error } = routeParamsSchema.validate({ id: 'unsupported' })
+      const { error } = paramsSchema.validate({ id: 'unsupported' })
       expect(error).to.have.property('message', '"metric" must be one of [pageViews, sessions, avgSessionDuration]')
     })
 
     it('should allow metric params', () => {
       ['pageViews', 'sessions', 'avgSessionDuration'].forEach(metric => {
-        const { error, value } = routeParamsSchema.validate({ id: metric })
+        const { error, value } = paramsSchema.validate({ id: metric })
         expect(error).to.be.an('undefined')
-        expect(value).to.deep.equal({
-          dimension: null,
-          metric
-        })
+        expect(value).to.have.property('dimension', null)
+        expect(value).to.have.property('metric', metric)
+        expect(value).to.have.property('time').to.be.an('object')
+        expect(value.time).to.have.property('startDate').and.be.a('string')
+        expect(iso8601Regex.test(value.time.startDate)).to.equal(true)
+        expect(value.time).to.have.property('endDate').and.be.a('string')
+        expect(iso8601Regex.test(value.time.endDate)).to.equal(true)
       })
     })
 
     it('should reject invalid dimension param', () => {
-      const { error } = routeParamsSchema.validate({ id: 'pageViews:unsupported' })
+      const { error } = paramsSchema.validate({ id: 'pageViews:unsupported' })
       expect(error).to.have.property('message', '"dimension" must be [day]')
     })
 
     it('should allow valid dimension param', () => {
-      const { error, value } = routeParamsSchema.validate({ id: 'pageViews:day' })
+      const { error, value } = paramsSchema.validate({ id: 'pageViews:day' })
       expect(error).to.be.an('undefined')
-      expect(value).to.deep.equal({
-        dimension: 'day',
-        metric: 'pageViews'
-      })
+      expect(value).to.have.property('dimension', 'day')
+      expect(value).to.have.property('metric', 'pageViews')
+      expect(value).to.have.property('time').to.be.an('object')
+      expect(value.time).to.have.property('startDate').and.be.a('string')
+      expect(iso8601Regex.test(value.time.startDate)).to.equal(true)
+      expect(value.time).to.have.property('endDate').and.be.a('string')
+      expect(iso8601Regex.test(value.time.endDate)).to.equal(true)
     })
   })
 })
