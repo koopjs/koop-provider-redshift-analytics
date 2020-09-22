5f6af72e9171028e18598478a9aae0df79fc588e
Rich <rich.gwozdz@gmail.com>
2020-09-22 12:05:06 -0700

    Implement time coercion.

--- test/schema/time.test.js
+++ test/schema/time.test.js
@@ -1,8 +1,7 @@
 /* eslint-env mocha */
 const chai = require('chai')
 const expect = chai.expect
-const joi = require('joi')
-const timeValidator = require('../../lib/schema/time')(joi)
+const { coerce: { method: coerceTime } } = require('../../lib/schema/time')
 const iso8601Regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/
 
 function getDayRange (start, end) {
@@ -13,34 +12,31 @@ function getDayRange (start, end) {
 
 describe('timeValidation', () => {
   it('should reject invalid range: "null"', () => {
-    const coerce = timeValidator.coerce.bind({
-      createError: (msg) => {
+    coerceTime('null', {
+      message: (msg) => {
         expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
       }
     })
-    coerce('null')
   })
 
   it('should reject invalid range: "2020-01-01"', () => {
-    const coerce = timeValidator.coerce.bind({
-      createError: (msg) => {
+    coerceTime('2020-01-01', {
+      message: (msg) => {
         expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
       }
     })
-    coerce('2020-01-01')
   })
 
   it('should reject invalid range: "hello,world"', () => {
-    const coerce = timeValidator.coerce.bind({
-      createError: (msg) => {
+    coerceTime('hello,world', {
+      message: (msg) => {
         expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
       }
     })
-    coerce('hello,world')
   })
 
   it('should coerce "null,null" to default ISO8601 time range', () => {
-    const result = timeValidator.coerce('null,null')
+    const { value: result } = coerceTime('null,null')
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
@@ -49,7 +45,7 @@ describe('timeValidation', () => {
   })
 
   it('should coerce "null,1600731000000" to a ISO8601 time range', () => {
-    const result = timeValidator.coerce('null,null')
+    const { value: result } = coerceTime('null,null')
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
@@ -58,7 +54,7 @@ describe('timeValidation', () => {
   })
 
   it('should coerce "null,1600731000000" to default ISO8601 time range', () => {
-    const result = timeValidator.coerce('null,1600731000000')
+    const { value: result } = coerceTime('null,1600731000000')
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
@@ -68,7 +64,7 @@ describe('timeValidation', () => {
 
   it('should coerce "<7 days ago>,null" to default ISO8601 time range', () => {
     const start = Date.now() - (7 * 24 * 60 * 60 * 1000)
-    const result = timeValidator.coerce(`${start},null`)
+    const { value: result } = coerceTime(`${start},null`)
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
@@ -79,7 +75,7 @@ describe('timeValidation', () => {
   it('should coerce "<ISO date>,<ISO date>" to default ISO8601 time range', () => {
     const end = Date.now()
     const start = end - (30 * 24 * 60 * 60 * 1000)
-    const result = timeValidator.coerce(`${(new Date(start)).toISOString()},${(new Date(end)).toISOString()}`)
+    const { value: result } = coerceTime(`${(new Date(start)).toISOString()},${(new Date(end)).toISOString()}`)
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
@@ -88,7 +84,7 @@ describe('timeValidation', () => {
   })
 
   it('should coerce "<YYYY-MM-DD>,<YYYY-MM-DD>" to default ISO8601 time range', () => {
-    const result = timeValidator.coerce('2020-04-01,2020-05-01')
+    const { value: result } = coerceTime('2020-04-01,2020-05-01')
     expect(result).to.have.property('startDate')
     expect(iso8601Regex.test(result.startDate)).to.equal(true)
     expect(result).to.have.property('endDate')
