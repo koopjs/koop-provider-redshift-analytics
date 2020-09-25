5cddabcfd256795034a4d0a65ee9851d1c519725
Rich <rich.gwozdz@gmail.com>
2020-09-25 08:41:41 -0700

    Reduce scope for PR review.

--- test/schema/index.test.js
+++ test/schema/index.test.js
@@ -2,7 +2,6 @@
 const chai = require('chai')
 const expect = chai.expect
 const { paramsSchema } = require('../../lib/schema')
-const iso8601Regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/
 
 describe('schema', function () {
   it('should reject invalid metric param', () => {
@@ -16,11 +15,6 @@ describe('schema', function () {
       expect(error).to.be.an('undefined')
       expect(value).to.have.property('dimension', null)
       expect(value).to.have.property('metric', metric)
-      expect(value).to.have.property('time').to.be.an('object')
-      expect(value.time).to.have.property('startDate').and.be.a('string')
-      expect(iso8601Regex.test(value.time.startDate)).to.equal(true)
-      expect(value.time).to.have.property('endDate').and.be.a('string')
-      expect(iso8601Regex.test(value.time.endDate)).to.equal(true)
     })
   })
 
@@ -34,38 +28,5 @@ describe('schema', function () {
     expect(error).to.be.an('undefined')
     expect(value).to.have.property('dimension', 'day')
     expect(value).to.have.property('metric', 'pageViews')
-    expect(value).to.have.property('time').to.be.an('object')
-    expect(value.time).to.have.property('startDate').and.be.a('string')
-    expect(iso8601Regex.test(value.time.startDate)).to.equal(true)
-    expect(value.time).to.have.property('endDate').and.be.a('string')
-    expect(iso8601Regex.test(value.time.endDate)).to.equal(true)
-  })
-
-  it('should allow valid time param', () => {
-    const start = (new Date(0)).toISOString()
-    const end = (new Date()).toISOString()
-    const { error, value } = paramsSchema.validate({ id: 'pageViews', time: `${start},${end}` })
-    expect(error).to.be.an('undefined')
-    expect(value).to.have.property('dimension', null)
-    expect(value).to.have.property('metric', 'pageViews')
-    expect(value).to.have.property('time').to.be.an('object')
-    expect(value.time).to.have.property('startDate').and.be.a('string')
-    expect(iso8601Regex.test(value.time.startDate)).to.equal(true)
-    expect(value.time).to.have.property('endDate').and.be.a('string')
-    expect(iso8601Regex.test(value.time.endDate)).to.equal(true)
-  })
-
-  it('should escape a where param', () => {
-    const start = (new Date(0)).toISOString()
-    const end = (new Date()).toISOString()
-    const { error, value } = paramsSchema.validate({
-      id: 'pageViews',
-      time: `${start},${end}`,
-      where: 'test\'; DROP table foo;--'
-    })
-    expect(error).to.be.an('undefined')
-    expect(value).to.have.property('dimension', null)
-    expect(value).to.have.property('metric', 'pageViews')
-    expect(value).to.have.property('where', '\'test\\\'; DROP table foo;--\'')
   })
 })
