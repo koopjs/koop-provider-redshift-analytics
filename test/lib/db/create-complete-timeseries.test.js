1a9f27a8b61c658a484f036b0ec8db0d94925f19
Rich <rich.gwozdz@gmail.com>
2020-09-28 13:40:56 -0700

    Zero value metrics.

--- test/lib/db/create-complete-timeseries.test.js
+++ test/lib/db/create-complete-timeseries.test.js
@@ -4,7 +4,7 @@ const expect = chai.expect
 const createCompleteTimeseries = require('../../../lib/db/create-complete-timeseries')
 const data = [
   {
-    metric: 80,
+    pageViews: 80,
     timestamp: new Date('2020-08-02T07:00:00.000Z')
   }
 ]
@@ -12,19 +12,22 @@ describe('createCompleteTimeseries', () => {
   it('should fill a timeseries that has missing timesteps', () => {
     const result = createCompleteTimeseries({
       data,
+      metric: 'pageViews',
       startDate: '2020-08-01T07:00:00.000Z',
       endDate: '2020-08-03T07:00:00.000Z',
       interval: 'day'
     })
     expect(result).to.deep.equal([
       {
+        pageViews: 0,
         timestamp: new Date('2020-08-01T07:00:00.000Z')
       },
       {
-        metric: 80,
+        pageViews: 80,
         timestamp: new Date('2020-08-02T07:00:00.000Z')
       },
       {
+        pageViews: 0,
         timestamp: new Date('2020-08-03T07:00:00.000Z')
       }
     ])
