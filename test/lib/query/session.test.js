50df1f43f07d1e4ef8fe2042e8621caf215eb3c3
Rich <rich.gwozdz@gmail.com>
2020-09-28 08:17:19 -0700

    Provider development part 2.

---
+++ test/lib/query/session.test.js
@@ -0,0 +1,67 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const proxyquire = require('proxyquire').noCallThru()
+const sinon = require('sinon')
+const expect = chai.expect
+const modulePath = '../../../lib/query/session'
+const configStub = {
+  koopProviderRedshiftAnalytics: {
+    redshift: {
+      schema: 'redshift-schema',
+      table: 'analytics-table',
+      sessionColumn: 'session-column',
+      eventTimestampColumn: 'timestamp-column'
+    },
+    timeDimensions: ['day']
+  }
+}
+
+describe('session query builder', () => {
+  it('should build a non-dimensioned session count query', () => {
+    const rawWhereStub = sinon.stub().returns('raw-where-clause')
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub,
+      './helpers/raw-where': rawWhereStub
+    })
+    const query = buildSessionQuery()
+    expect(query.toString()).to.equal('select COUNT(DISTINCT session-column) AS sessions from "redshift-schema"."analytics-table" where raw-where-clause')
+    expect(rawWhereStub.calledOnce).to.equal(true)
+    expect(rawWhereStub.firstCall.args).to.deep.equal([{ endDate: undefined, startDate: undefined, where: undefined }])
+  })
+
+  it('should build a timestamp-dimensioned session count query', () => {
+    const rawWhereStub = sinon.stub().returns('raw-where-clause')
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub,
+      './helpers/raw-where': rawWhereStub
+    })
+    const query = buildSessionQuery({ dimension: 'day' })
+    expect(query.toString()).to.equal('select DATE_TRUNC(\'day\', timestamp-column ) AS timestamp, COUNT(DISTINCT session-column) AS sessions from "redshift-schema"."analytics-table" where raw-where-clause group by DATE_TRUNC(\'day\', timestamp-column ) order by DATE_TRUNC(\'day\', timestamp-column )')
+    expect(rawWhereStub.calledOnce).to.equal(true)
+    expect(rawWhereStub.firstCall.args).to.deep.equal([{ endDate: undefined, startDate: undefined, where: undefined }])
+  })
+
+  it('should build a non-timestamp-dimensioned session count query', () => {
+    const rawWhereStub = sinon.stub().returns('raw-where-clause')
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub,
+      './helpers/raw-where': rawWhereStub
+    })
+    const query = buildSessionQuery({ dimension: 'a_label' })
+    expect(query.toString()).to.equal('select "a_label", COUNT(DISTINCT session-column) AS sessions from "redshift-schema"."analytics-table" where raw-where-clause group by "a_label"')
+    expect(rawWhereStub.calledOnce).to.equal(true)
+    expect(rawWhereStub.firstCall.args).to.deep.equal([{ endDate: undefined, startDate: undefined, where: undefined }])
+  })
+
+  it('should build a non-dimensioned session count query with addition where fragment', () => {
+    const rawWhereStub = sinon.stub().returns('raw-where-clause')
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub,
+      './helpers/raw-where': rawWhereStub
+    })
+    const query = buildSessionQuery({ startDate: 'START', endDate: 'END', where: 'x=\'y\'' })
+    expect(query.toString()).to.equal('select COUNT(DISTINCT session-column) AS sessions from "redshift-schema"."analytics-table" where raw-where-clause')
+    expect(rawWhereStub.calledOnce).to.equal(true)
+    expect(rawWhereStub.firstCall.args).to.deep.equal([{ startDate: 'START', endDate: 'END', where: 'x=\'y\'' }])
+  })
+})
---
+++ test/schema/time.test.js
@@ -0,0 +1,94 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const expect = chai.expect
+const { coerce: { method: coerceTime } } = require('../../lib/schema/time')
+const iso8601Regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/
+
+function getDayRange (start, end) {
+  const startDate = new Date(start)
+  const endDate = new Date(end)
+  return Math.round((endDate - startDate) / (24 * 60 * 60 * 1000))
+}
+
+describe('timeValidation', () => {
+  it('should reject invalid range: "null"', () => {
+    coerceTime('null', {
+      message: (msg) => {
+        expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
+      }
+    })
+  })
+
+  it('should reject invalid range: "2020-01-01"', () => {
+    coerceTime('2020-01-01', {
+      message: (msg) => {
+        expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
+      }
+    })
+  })
+
+  it('should reject invalid range: "hello,world"', () => {
+    coerceTime('hello,world', {
+      message: (msg) => {
+        expect(msg).to.equal('"time" param must be a comma delimited string: "<start>,<end>". Use "null", an ISO Date srting, YYYY-MM-DD string, or a unix timestamp')
+      }
+    })
+  })
+
+  it('should coerce "null,null" to default ISO8601 time range', () => {
+    const { value: result } = coerceTime('null,null')
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(30)
+  })
+
+  it('should coerce "null,1600731000000" to a ISO8601 time range', () => {
+    const { value: result } = coerceTime('null,null')
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(30)
+  })
+
+  it('should coerce "null,1600731000000" to default ISO8601 time range', () => {
+    const { value: result } = coerceTime('null,1600731000000')
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(30)
+  })
+
+  it('should coerce "<7 days ago>,null" to default ISO8601 time range', () => {
+    const start = Date.now() - (7 * 24 * 60 * 60 * 1000)
+    const { value: result } = coerceTime(`${start},null`)
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(7)
+  })
+
+  it('should coerce "<ISO date>,<ISO date>" to default ISO8601 time range', () => {
+    const end = Date.now()
+    const start = end - (30 * 24 * 60 * 60 * 1000)
+    const { value: result } = coerceTime(`${(new Date(start)).toISOString()},${(new Date(end)).toISOString()}`)
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(30)
+  })
+
+  it('should coerce "<YYYY-MM-DD>,<YYYY-MM-DD>" to default ISO8601 time range', () => {
+    const { value: result } = coerceTime('2020-04-01,2020-05-01')
+    expect(result).to.have.property('startDate')
+    expect(iso8601Regex.test(result.startDate)).to.equal(true)
+    expect(result).to.have.property('endDate')
+    expect(iso8601Regex.test(result.endDate)).to.equal(true)
+    expect(getDayRange(result.startDate, result.endDate)).to.equal(30)
+  })
+})
