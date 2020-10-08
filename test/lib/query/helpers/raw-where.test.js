18d0e405b92278f1ff724cda692d37ec50e87f36
Rich <rich.gwozdz@gmail.com>
2020-10-08 11:07:36 -0700

    Extract raw-where building and pass in as param.

--- test/lib/query/helpers/raw-where.test.js
+++ test/lib/query/helpers/raw-where.test.js
@@ -1,11 +1,45 @@
 /* eslint-env mocha */
 const chai = require('chai')
 const expect = chai.expect
-const buildRawWhere = require('../../../../lib/query/helpers/raw-where')
+const proxyquire = require('proxyquire').noCallThru()
+const modulePath = '../../../../lib/query/helpers/raw-where'
+const _ = require('lodash')
+const configStub = {
+  koopProviderRedshiftAnalytics: {
+    redshift: {
+      sources: {
+        defaultSource: {
+          timestampColumn: 'default-timestamp-column'
+        }
+      }
+    }
+  }
+}
 
 describe('buildRawWhere', () => {
-  it('should build where clause with all options', () => {
-    const result = buildRawWhere({ startDate: 'START_DATE', endDate: 'END_DATE', where: '1 = 2', timestampColumn: 'timestamp-column' })
-    expect(result).to.equal('timestamp-column > \'START_DATE\' AND timestamp-column <= \'END_DATE\' AND 1 = 2')
+  it('should build where clause for session metric with default source', () => {
+    const buildRawWhere = proxyquire(modulePath, { config: configStub })
+    const result = buildRawWhere({ metric: 'sessions', startDate: 'START_DATE', endDate: 'END_DATE', where: '1 = 2' })
+    expect(result).to.equal('default-timestamp-column > \'START_DATE\' AND default-timestamp-column <= \'END_DATE\' AND 1 = 2')
+  })
+
+  it('should build where clause for event metric with default source', () => {
+    const buildRawWhere = proxyquire(modulePath, { config: configStub })
+    const result = buildRawWhere({ metric: 'event', startDate: 'START_DATE', endDate: 'END_DATE', where: '1 = 2' })
+    expect(result).to.equal('default-timestamp-column > \'START_DATE\' AND default-timestamp-column <= \'END_DATE\' AND 1 = 2')
+  })
+
+  it('should build where clause for session metric with session source', () => {
+    _.set(configStub, 'koopProviderRedshiftAnalytics.redshift.sources.session.timestamp', 'session-timestamp-column')
+    const buildRawWhere = proxyquire(modulePath, { config: configStub })
+    const result = buildRawWhere({ metric: 'sessions', startDate: 'START_DATE', endDate: 'END_DATE', where: '1 = 2' })
+    expect(result).to.equal('session-timestamp-column > \'START_DATE\' AND session-timestamp-column <= \'END_DATE\' AND 1 = 2')
+  })
+
+  it('should build where clause for event metric with event source', () => {
+    _.set(configStub, 'koopProviderRedshiftAnalytics.redshift.sources.event.timestamp', 'event-timestamp-column')
+    const buildRawWhere = proxyquire(modulePath, { config: configStub })
+    const result = buildRawWhere({ metric: 'event', startDate: 'START_DATE', endDate: 'END_DATE', where: '1 = 2' })
+    expect(result).to.equal('event-timestamp-column > \'START_DATE\' AND event-timestamp-column <= \'END_DATE\' AND 1 = 2')
   })
 })
--- test/lib/query/session-duration.test.js
+++ test/lib/query/session-duration.test.js
@@ -1,7 +1,6 @@
 /* eslint-env mocha */
 const chai = require('chai')
 const proxyquire = require('proxyquire').noCallThru()
-const sinon = require('sinon')
 const expect = chai.expect
 const modulePath = '../../../lib/query/session-duration'
 const configStub = {
@@ -22,50 +21,26 @@ const configStub = {
 
 describe('session-duration query builder', () => {
   it('should build a non-dimensioned session duration query', () => {
-    const rawWhereStub = sinon.stub().returns('raw-where-clause')
     const buildSessionDurationQuery = proxyquire(modulePath, {
-      config: configStub,
-      './helpers/raw-where': rawWhereStub
+      config: configStub
     })
-    const query = buildSessionDurationQuery({ time: { startDate: 'START', endDate: 'END' } })
+    const query = buildSessionDurationQuery({ time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select avg("session_duration") as "avg_session_duration" from (select "session-column", (CASE WHEN COUNT(*) > 1 THEN CAST(DATEDIFF(second, MIN(timestamp-column), MAX(timestamp-column)) AS FLOAT) END) AS session_duration, max("timestamp-column") as "session_end" from "redshift-schema"."analytics-table" where raw-where-clause group by "session-column")')
-    expect(rawWhereStub.calledOnce).to.equal(true)
-    expect(rawWhereStub.firstCall.args).to.deep.equal([{ startDate: 'START', endDate: 'END', where: undefined, timestampColumn: 'timestamp-column' }])
   })
 
   it('should build a timestamp-dimensioned session duration query', () => {
-    const rawWhereStub = sinon.stub().returns('raw-where-clause')
     const buildSessionDurationQuery = proxyquire(modulePath, {
-      config: configStub,
-      './helpers/raw-where': rawWhereStub
+      config: configStub
     })
-    const query = buildSessionDurationQuery({ dimension: 'day', time: { startDate: 'START', endDate: 'END' } })
+    const query = buildSessionDurationQuery({ dimension: 'day', time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select avg("session_duration") as "avg_session_duration", DATE_TRUNC(\'day\', session_end ) AS timestamp from (select "session-column", (CASE WHEN COUNT(*) > 1 THEN CAST(DATEDIFF(second, MIN(timestamp-column), MAX(timestamp-column)) AS FLOAT) END) AS session_duration, max("timestamp-column") as "session_end" from "redshift-schema"."analytics-table" where raw-where-clause group by "session-column") group by DATE_TRUNC(\'day\', session_end)')
-    expect(rawWhereStub.calledOnce).to.equal(true)
-    expect(rawWhereStub.firstCall.args).to.deep.equal([{ startDate: 'START', endDate: 'END', where: undefined, timestampColumn: 'timestamp-column' }])
   })
 
   it('should build a non-timestamp-dimensioned session duration query', () => {
-    const rawWhereStub = sinon.stub().returns('raw-where-clause')
     const buildSessionDurationQuery = proxyquire(modulePath, {
-      config: configStub,
-      './helpers/raw-where': rawWhereStub
+      config: configStub
     })
-    const query = buildSessionDurationQuery({ dimension: 'a_hostname', time: { startDate: 'START', endDate: 'END' } })
+    const query = buildSessionDurationQuery({ dimension: 'a_hostname', time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select avg("session_duration") as "avg_session_duration", "a_hostname" from (select "a_hostname", "session-column", (CASE WHEN COUNT(*) > 1 THEN CAST(DATEDIFF(second, MIN(timestamp-column), MAX(timestamp-column)) AS FLOAT) END) AS session_duration, max("timestamp-column") as "session_end" from "redshift-schema"."analytics-table" where raw-where-clause group by "a_hostname", "session-column") group by "a_hostname"')
-    expect(rawWhereStub.calledOnce).to.equal(true)
-    expect(rawWhereStub.firstCall.args).to.deep.equal([{ startDate: 'START', endDate: 'END', where: undefined, timestampColumn: 'timestamp-column' }])
-  })
-
-  it('should build a non-dimensioned session count query with addition where fragment', () => {
-    const rawWhereStub = sinon.stub().returns('raw-where-clause')
-    const buildSessionDurationQuery = proxyquire(modulePath, {
-      config: configStub,
-      './helpers/raw-where': rawWhereStub
-    })
-    const query = buildSessionDurationQuery({ time: { startDate: 'START', endDate: 'END' }, where: 'x=\'y\'' })
-    expect(query.toString()).to.equal('select avg("session_duration") as "avg_session_duration" from (select "session-column", (CASE WHEN COUNT(*) > 1 THEN CAST(DATEDIFF(second, MIN(timestamp-column), MAX(timestamp-column)) AS FLOAT) END) AS session_duration, max("timestamp-column") as "session_end" from "redshift-schema"."analytics-table" where raw-where-clause group by "session-column")')
-    expect(rawWhereStub.calledOnce).to.equal(true)
-    expect(rawWhereStub.firstCall.args).to.deep.equal([{ startDate: 'START', endDate: 'END', where: 'x=\'y\'', timestampColumn: 'timestamp-column' }])
   })
 })
