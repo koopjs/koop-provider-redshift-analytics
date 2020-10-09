64abadcbf740d24ee4d00d282f33b96062526507
Rich <rich.gwozdz@gmail.com>
2020-10-09 08:29:24 -0700

    Transpose and aggregate.

--- test/lib/query/event.test.js
+++ test/lib/query/event.test.js
@@ -26,17 +26,26 @@ const buildEventQuery = proxyquire(modulePath, {
 
 describe('event query builder', () => {
   it('should build a non-dimensioned event count query', () => {
-    const query = buildEventQuery({ metric: 'pageViews', time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
+    const buildEventQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildEventQuery({ metric: 'pageViews', startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select count("event-column") as "page_views" from "redshift-schema"."analytics-table" where "event-column" = \'pageView\' and raw-where-clause')
   })
 
   it('should build a timestamp-dimensioned event count query', () => {
-    const query = buildEventQuery({ metric: 'pageViews', dimension: 'day', time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
+    const buildEventQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildEventQuery({ metric: 'pageViews', timeDimension: 'day', nonTimeDimensions: [], dimensions: ['day'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select DATE_TRUNC(\'day\', timestamp-column ) AS timestamp, count("event-column") as "page_views" from "redshift-schema"."analytics-table" where "event-column" = \'pageView\' and raw-where-clause group by DATE_TRUNC(\'day\', timestamp-column ) order by DATE_TRUNC(\'day\', timestamp-column )')
   })
 
   it('should build a non-timestamp-dimensioned event count query', () => {
-    const query = buildEventQuery({ metric: 'pageViews', dimension: 'a_label', time: { startDate: 'START', endDate: 'END' }, where: 'raw-where-clause' })
+    const buildEventQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildEventQuery({ metric: 'pageViews', dimensions: ['a_label'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select "a_label", count("event-column") as "page_views" from "redshift-schema"."analytics-table" where "event-column" = \'pageView\' and raw-where-clause group by "a_label"')
   })
 })
