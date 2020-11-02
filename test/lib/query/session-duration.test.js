95a9c94cc4176b8fbd7c9272fa33607f76333f65
Rich Gwozdz <rich.gwozdz@gmail.com>
2020-11-02 06:11:08 -0800

    Allow use of separate table/view for session queries if one is available (#8)
    * Allow use of separate table/view for session queries if one is available.
    
    * Change check syntax.

--- test/lib/query/session-duration.test.js
+++ test/lib/query/session-duration.test.js
@@ -3,23 +3,24 @@ const chai = require('chai')
 const proxyquire = require('proxyquire').noCallThru()
 const expect = chai.expect
 const modulePath = '../../../lib/query/session-duration'
-const configStub = {
-  koopProviderRedshiftAnalytics: {
-    redshift: {
-      sources: {
-        session: {
-          schema: 'redshift-schema',
-          table: 'analytics-table',
-          sessionColumn: 'session-column',
-          timestampColumn: 'timestamp-column'
-        }
-      }
-    },
-    timeDimensions: ['day']
-  }
-}
 
-describe('session-duration query builder', () => {
+describe('session-duration query builder target default source', () => {
+  const configStub = {
+    koopProviderRedshiftAnalytics: {
+      redshift: {
+        sources: {
+          defaultSource: {
+            schema: 'redshift-schema',
+            table: 'analytics-table',
+            sessionColumn: 'session-column',
+            timestampColumn: 'timestamp-column'
+          }
+        }
+      },
+      timeDimensions: ['day']
+    }
+  }
+
   it('should build a non-dimensioned session duration query', () => {
     const buildSessionDurationQuery = proxyquire(modulePath, {
       config: configStub
@@ -42,5 +43,47 @@ describe('session-duration query builder', () => {
     })
     const query = buildSessionDurationQuery({ dimensions: ['a_hostname'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select avg("session_duration") as "avg_session_duration", "a_hostname" from (select "a_hostname", "session-column", (CASE WHEN COUNT(*) > 1 THEN CAST(DATEDIFF(second, MIN(timestamp-column), MAX(timestamp-column)) AS FLOAT) END) AS session_duration, max("timestamp-column") as "session_end" from "redshift-schema"."analytics-table" where raw-where-clause group by "a_hostname", "session-column") group by "a_hostname"')
+  })
+})
+
+describe('session-duration query builder targeting session view', () => {
+  const configStub = {
+    koopProviderRedshiftAnalytics: {
+      redshift: {
+        sources: {
+          session: {
+            schema: 'redshift-schema',
+            table: 'session-view',
+            sessionDurationColumn: 'session-duration-column',
+            timestampColumn: 'timestamp-column'
+          }
+        }
+      },
+      timeDimensions: ['day']
+    }
+  }
+
+  it('should build a non-dimensioned session duration query', () => {
+    const buildSessionDurationQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionDurationQuery({ startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select avg("session-duration-column") as "avg_session_duration" from "redshift-schema"."session-view" where raw-where-clause')
+  })
+
+  it('should build a time-dimensioned session duration query', () => {
+    const buildSessionDurationQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionDurationQuery({ timeDimension: 'day', dimensions: ['day'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select avg("session-duration-column") as "avg_session_duration", DATE_TRUNC(\'day\', timestamp-column) AS timestamp from "redshift-schema"."session-view" where raw-where-clause group by DATE_TRUNC(\'day\', timestamp-column)')
+  })
+
+  it('should build a non-time-dimensioned session duration query', () => {
+    const buildSessionDurationQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionDurationQuery({ dimensions: ['a_hostname'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select avg("session-duration-column") as "avg_session_duration", "a_hostname" from "redshift-schema"."session-view" where raw-where-clause group by "a_hostname"')
   })
 })
--- test/lib/query/session.test.js
+++ test/lib/query/session.test.js
@@ -3,23 +3,23 @@ const chai = require('chai')
 const proxyquire = require('proxyquire').noCallThru()
 const expect = chai.expect
 const modulePath = '../../../lib/query/session'
-const configStub = {
-  koopProviderRedshiftAnalytics: {
-    redshift: {
-      sources: {
-        session: {
-          schema: 'redshift-schema',
-          table: 'analytics-table',
-          sessionColumn: 'session-column',
-          timestampColumn: 'timestamp-column'
-        }
-      }
-    },
-    timeDimensions: ['day']
-  }
-}
 
-describe('session query builder', () => {
+describe('session query builder using default source', () => {
+  const configStub = {
+    koopProviderRedshiftAnalytics: {
+      redshift: {
+        sources: {
+          defaultSource: {
+            schema: 'redshift-schema',
+            table: 'analytics-table',
+            sessionColumn: 'session-column',
+            timestampColumn: 'timestamp-column'
+          }
+        }
+      },
+      timeDimensions: ['day']
+    }
+  }
   it('should build a non-dimensioned session count query', () => {
     const buildSessionQuery = proxyquire(modulePath, {
       config: configStub
@@ -42,5 +42,46 @@ describe('session query builder', () => {
     })
     const query = buildSessionQuery({ dimensions: ['a_label'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
     expect(query.toString()).to.equal('select "a_label", COUNT(DISTINCT session-column) AS sessions from "redshift-schema"."analytics-table" where raw-where-clause group by "a_label"')
+  })
+})
+
+describe('session query builder using session source', () => {
+  const configStub = {
+    koopProviderRedshiftAnalytics: {
+      redshift: {
+        sources: {
+          session: {
+            schema: 'redshift-schema',
+            table: 'session-view',
+            sessionColumn: 'session-column',
+            timestampColumn: 'timestamp-column'
+          }
+        }
+      },
+      timeDimensions: ['day']
+    }
+  }
+  it('should build a non-dimensioned session count query', () => {
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionQuery({ startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select COUNT(*) AS sessions from "redshift-schema"."session-view" where raw-where-clause')
+  })
+
+  it('should build a timestamp-dimensioned session count query', () => {
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionQuery({ timeDimension: 'day', dimensions: ['day'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select DATE_TRUNC(\'day\', timestamp-column ) AS timestamp, COUNT(*) AS sessions from "redshift-schema"."session-view" where raw-where-clause group by DATE_TRUNC(\'day\', timestamp-column ) order by DATE_TRUNC(\'day\', timestamp-column )')
+  })
+
+  it('should build a non-timestamp-dimensioned session count query', () => {
+    const buildSessionQuery = proxyquire(modulePath, {
+      config: configStub
+    })
+    const query = buildSessionQuery({ dimensions: ['a_label'], startDate: 'START', endDate: 'END', where: 'raw-where-clause' })
+    expect(query.toString()).to.equal('select "a_label", COUNT(*) AS sessions from "redshift-schema"."session-view" where raw-where-clause group by "a_label"')
   })
 })
